c783d33dde84aad4cceb69922dc76f891030fd3c
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-05-14 15:45:20 +0530

    Add files via upload

---
+++ vowcon.py
@@ -0,0 +1,7 @@
+num = str(input())
+if num == "a" or num == "e"  or num == "i" or num =="o" or num == "u":
+	print("The string is vowels")
+elif num.isalpha():
+	print("The String is consonant")
+else:
+	print("Invalid")
