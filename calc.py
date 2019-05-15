d8d9cf218b1d06460ff228f2825fa1ed2424777a
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-05-15 09:55:24 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,33 @@
+a=int(input())
+
+b=int(input())
+
+c=input("Enter the operation")
+
+if c=="1":
+    
+	c=a+b
+    
+	print(c)
+
+elif c=="2":
+    
+	c=a-b
+    
+	print(c)
+
+elif c=="3":
+    
+	if b == 0:
+        
+		print("Infinite")
+    
+	else:
+        
+		c=a/b
+        
+		print(c)
+
+else:
+    
+	print("No operation is found")
