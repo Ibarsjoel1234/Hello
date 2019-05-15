1ad20e757500e371168c85613066e835527c3a4d
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-05-15 10:59:42 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,33 +1,38 @@
-a=int(input())
-
-b=int(input())
-
+from decimal import *
+a=str(input())
+b=str(input())
 c=input("Enter the operation")
-
 if c=="1":
-    
-	c=a+b
-    
-	print(c)
-
+    if a.isalpha():
+        print("Throw exception: a")
+    elif b.isalpha():
+        print("Throw exception: b")
+    else:
+        a=Decimal(a)
+        b=Decimal(b)
+        c=a+b
+        print(c)
 elif c=="2":
-    
-	c=a-b
-    
-	print(c)
-
+  if a.isalpha():
+    print("Throw expection : a")
+  elif b.isalpha():
+    print("Thow expection: b")
+  else:
+    a=Decimal(a)
+    b=Decimal(b)
+    c=a-b
+    print(c)
 elif c=="3":
-    
-	if b == 0:
-        
-		print("Infinite")
-    
-	else:
-        
-		c=a/b
-        
-		print(c)
-
+  if a.isalpha():
+      print("Throw exception: a")
+  elif b.isalpha():
+      print("Throw expection: b")
+  if b == 0:
+      print("Infinite")
+  else:
+      a=Decimal(a)
+      b=Decimal(b)
+      c=a/b
+      print(c)
 else:
-    
-	print("No operation is found")
+    print("No operation is found")
