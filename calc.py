e92df8c1333c7655476a9adea846e9fc3742bbd1
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-05-16 21:56:12 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,19 +1,30 @@
 from decimal import *
 a=str(input())
 b=str(input())
-c=input("Enter the operation")
+#if a.isalpha() and b.isalpha():
+ # print("Both a and b are alphabet")
+  #exit()
+c=input("Enter the operation\t")
 if c=="1":
-    if a.isalpha():
+    if a.isalpha and b.isalpha():
+      print("Both a and b are expection")
+      exit()
+    elif a.isalpha():
         print("Throw exception: a")
     elif b.isalpha():
         print("Throw exception: b")
+    elif a.isalpha() and b.isalpha():
+        print("Both a and b is alphabet")
     else:
         a=Decimal(a)
         b=Decimal(b)
         c=a+b
         print(c)
 elif c=="2":
-  if a.isalpha():
+  if a.isalpha() and b.isalpha():
+    print("Both a and b are alphabet")
+    exit()
+  elif a.isalpha():
     print("Throw expection : a")
   elif b.isalpha():
     print("Thow expection: b")
@@ -23,7 +34,9 @@ elif c=="2":
     c=a-b
     print(c)
 elif c=="3":
-  if a.isalpha():
+  if a.isalpha() and b.isalpha():
+    print("Both a and b are alphabet")
+  elif a.isalpha():
       print("Throw exception: a")
   elif b.isalpha():
       print("Throw expection: b")
