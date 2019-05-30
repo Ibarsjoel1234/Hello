cfa7766a487350ce14896e3958af8a528e7bd418
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-05-30 19:11:12 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,9 @@
+no=int(input())
+if num>1:
+	for i in range(2,no)
+		if(no%i)==0:
+			print(num,"It is not a prime number")
+		else:
+			print(num,"It is a prime number")
+else:
+	print("It is not prime number")
