0cac5e615ec0e317fb5b6f541107bd5c1e057ecc
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-06-05 00:58:01 +0530

    Create notoword.py

---
+++ notoword.py
@@ -0,0 +1,21 @@
+nn=int(input())
+if nn==1:
+	print("One")
+elif nn==2:
+	print("Two")
+elif nn==3:
+	print("Three")
+elif nn==4:
+	print("Four")
+elif nn==5:
+	print("Five")
+elif nn==6:
+	print("Six")
+elif nn==7:
+	print("Seven")
+elif nn==8:
+	print("Eight")
+elif nn==9:
+	print("Nine")
+else:
+	print("Ten")
