c045262f5307dcc16ef047819197b8ebcce5f6ef
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-06-14 22:45:54 +0530

    Create squarernot.py

---
+++ squarernot.py
@@ -0,0 +1,13 @@
+no,no1=list(map(int,input().split()))
+no2,no3=list(map(int,input().split()))
+no4,no5=list(map(int,input().split()))
+no6,no7=list(map(int,input().split()))
+a=no4-no
+b=no5-no1
+c=no6-no2
+d=no7-no3
+if(a==b==c==d):
+    print("yes")
+else:
+    print("no")
+    
