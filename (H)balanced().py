b0a54528e52c61654a4a5bf89cf344b3a29a941c
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-06-22 12:17:24 +0530

    Create (H)balanced().py

---
+++ (H)balanced().py
@@ -0,0 +1,11 @@
+val = ['()','(())','()()()()']
+go = input()
+if go == val[0]:
+    print("yes")
+elif go == val[1]:
+    print("yes")
+elif go == val[2]:
+    print("yes")
+else:
+    print("no")
+
