235f5d22f61d074376d703f7e77b7dda52ff5e79
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-06-22 12:05:26 +0530

    Create (H)printhoildays.py

---
+++ (H)printhoildays.py
@@ -0,0 +1,28 @@
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+val = input()
+for hoil in days:
+    if val == days[0]:
+        print("no")
+        break
+    elif val == days[1]:
+        print("no")
+        break
+    elif val == days[2]:
+        print("no")
+        break
+    elif val == days[3]:
+        print("no")
+        break
+    elif val == days[4]:
+        print("no")
+        break
+    elif val == days[5]:
+        print("yes")
+        break
+    else:
+        if val == days[6]:
+            print("yes")
+            break
+
+
+
