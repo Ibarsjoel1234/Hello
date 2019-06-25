a533cd90071607e0d4a2d812d6489d8f0040c550
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-06-25 14:01:36 +0530

    Create (H)sortednumbers.py

---
+++ (H)sortednumbers.py
@@ -0,0 +1,11 @@
+val = input()
+need_val = input().split()
+bro = []
+for i in sorted(need_val):
+    bro.append(i)
+if bro == need_val:
+    print("yes")
+else:
+    print("no")
+    
+    #Hunter set 5 qns:42
