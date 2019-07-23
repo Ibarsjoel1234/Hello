7e16496dc1f160e1d1c1cb3bd3af494b131b636b
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2019-07-23 22:31:33 +0530

    Create floyd.py

---
+++ floyd.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the numbers: "))
+num = 1
+for row in range(1,n+1):
+    for col in range(1,row+1):
+        print(num,end=" ")
+        num = num + 1
+    print()
+    
+    
+    
+    
+    
+    #Floyd's Triangle | Printing Numbers in Right Triangle Shape
