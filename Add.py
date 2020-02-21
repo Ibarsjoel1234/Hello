b111e9913f74e446e8ef21cc9b568f1853c042c0
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2020-02-22 00:44:28 +0530

    Create Add.py

---
+++ Add.py
@@ -0,0 +1,11 @@
+num1 = int(input())
+num2 = int(input())
+res = solveval(num1,num2)
+print(res)
+
+
+def solveval(a,b):
+  c = a + b
+  return c
+  
+  # addition of two numbers using functions or accessing the function
