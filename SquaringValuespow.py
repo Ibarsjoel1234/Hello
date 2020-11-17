a8963b7287fe933d8d185ec2075e956214785d52
Ibarsjoel1234 <35426719+Ibarsjoel1234@users.noreply.github.com>
2020-11-17 14:47:13 +0530

    Using inbuilt functions to square the values

---
+++ SquaringValuespow.py
@@ -0,0 +1,13 @@
+def square(b):
+  c = pow(b,2)
+  return c
+
+
+a = int(input("Enter the vaules: "))
+b = square(a)
+print(b)
+
+Note:
+  pow() function is used to square the values
+  Example: if pow(5,2) where is the number we need to square
+            specified 2 to square
